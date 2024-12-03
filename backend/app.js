671452b9633c812abc8d42616244829c7b462ff8
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2024-12-03 13:35:15 +0530

    Created a test

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = new express()
+const ejs = require("ejs")
 
 app.use(express.static((__dirname, 'public')))
-
+app.set('view engine', 'ejs')
+app.set('views','./src/views')
 app.get('/',(req,res)=>{
-    res.send("connected")
+    res.render("index")
 })
 
 app.listen(3000,()=>{
