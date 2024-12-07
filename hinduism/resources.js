0714e61838190c5a8264babf26240ca3aa336aaa
Dhruv <sheredhruv@gmail.com>
2024-12-07 23:00:53 +0530

    Making Resource File and initializing object in it

---
+++ hinduism/resources.js
@@ -0,0 +1,10 @@
+import logo from './public/Resources/Logo.png';
+
+const images = [logo];
+const imageDetails = {
+    logo: {
+        src: logo,
+        alt: 'Company Logo',
+    },
+};
+export { images, imageDetails };
