415f3c7ceb224e1144b98622c26f5ed07e2f944f
Dhruv <sheredhruv@gmail.com>
2024-12-07 23:36:48 +0530

    Making Resource File and initializing object in it

--- hinduism/src/App.jsx
+++ hinduism/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { imageDetails } from "../resources";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -13,7 +14,11 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={logo} className="logo react" alt="React logo" />
+          <img
+            src={imageDetails.logo.src}
+            className="logo react"
+            alt={imageDetails.logo.alt}
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
