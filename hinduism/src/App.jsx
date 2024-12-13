041b025089ee52a72aa32b5b62f03e84d25d42c2
Neesargsoni <neesargs@gmail.com>
2024-12-13 00:31:44 -0700

    Navbar added

--- hinduism/src/App.jsx
+++ hinduism/src/App.jsx
@@ -1,39 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { imageDetails } from "../resources";
+// import { imageDetails } from "../resources";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "../Components/navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src={imageDetails.loading.src}
-            className="logo react"
-            alt={imageDetails.loading.alt}
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Navbar/>
   );
 }
 
