cf382840203457bea7534f2f898bb1d518052ab6
Dhruv <sheredhruv@gmail.com>
2025-01-11 23:53:29 +0530

    Changes as the theme

--- hinduism/Pages/Bhakti.jsx
+++ hinduism/Pages/Bhakti.jsx
@@ -61,7 +61,7 @@ const bhaktiTypes = [
   
 export default function Bhakti() {
   return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen  bg-gradient-to-b from-orange-100 to-yellow-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold text-gray-900 mb-4">Bhakti Sadhana</h1>
