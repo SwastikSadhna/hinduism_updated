fbef876bf526f1a89e734e61d039081a5cd0994b
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2025-01-18 22:39:26 +0530

    `Update multiple query files with new SQL queries and modifications`

--- backend/src/query/granthquries.js
+++ backend/src/query/granthquries.js
@@ -1,10 +1,14 @@
 const granthquery = {
     //1. get all granth
+    getAllGranth: `SELECT * FROM public."granth";`,
     //2. get granth_item by granth
+    getGranthDetails: `SELECT * FROM public."granth_item" WHERE "granth_id" = $1;`,
     //3. create
+    addGranth: `INSERT INTO public."granth" ("title", "description", "images") VALUES ($1,$2,$3);`,
     //4. update
+    updateGranth: `UPDATE public."granth" SET "title" = $1, "description" = $2, "images" = $3 WHERE "id" = $4;`,
     //5. delete
-    //6. insert
+    deleteGranth: `DELETE FROM public."granth" WHERE "id" = $1;`,
 }
 
 module.exports = {granthquery}
--- backend/src/query/slokaqueries.js
+++ backend/src/query/slokaqueries.js
@@ -1,12 +1,18 @@
 const slokaquery = {
     //1. get all sloka
+    getAllSloka: `SELECT * FROM public."slokas" ORDER BY "title"`,
     //2. get sloka with sloka_item
+    getSlokaDetails: `SELECT * FROM public."sloka_items" JOIN public."slokas" ON slokas._id = sloka_items.slok_id AND slokas._id = $1;`,
     //3. create
+    addSloka: `INSERT INTO public."slokas" ("title", "description", "keyword") VALUES ($1,$2,$3);`,
     //4. update
+    updateSloka: `UPDATE public."slokas" SET "title" = $1, "description" = $2, "keyword" = $3 WHERE "id" = $4;`,
     //5. delete
-    //6. insert
+    deleteSloka: `DELETE FROM public."slokas" WHERE "id" = $1`,
     //8. filter sloka by keyword
+    filterSloka: `SELECT * FROM public."slokas" WHERE `,
     //9. search sloka by title
+    searchSloka: `SELECT ""title","_id" FROM public."slokas" WHERE "title" ILIKE "$1*"`,
 }
 
 module.exports = {slokaquery}
