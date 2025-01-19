c4384dce6cb0eadf1e78e647d02353c649e66aaf
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2025-01-19 14:04:29 +0530

    `Added new routes and functionality to temple API: search, filter, get by id, and get Chardham temples. Updated temple model and queries to support new functionality.`

--- backend/src/controller/templeController.js
+++ backend/src/controller/templeController.js
@@ -1,3 +1,4 @@
+const { getFilterQuery } = require("../middleware/filterHelper")
 const Temple = require("../model/templeModel")
 
 const getAllTemples = async (req, res) => {
@@ -13,4 +14,56 @@ const getAllTemples = async (req, res) => {
     }
 }
 
-module.exports = {getAllTemples}
+const getTempleById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const temple = await Temple.getTempleById(id);
+        if (temple)
+            res.status(200).json(temple)
+        else
+            res.status(404).json({ message: "Temple not found" })
+    } catch (err) {
+        res.status(500).json({ message: "Error occurred while retrieving temple by id", error: err })
+    }
+}
+
+const searchTemple = async (req, res) => {
+    try {
+        const query = req.query.q;
+        const result = await Temple.searchTemple(query);
+        if (result.length > 0)
+            res.status(200).json(result)
+        else
+            res.status(404).json({ message: "No temples found for "+query })
+    } catch (err) {
+        res.status(500).json({ message: `Error occurred while searching temple query`, error: err })
+    }
+}
+
+const filterTemple = async (req, res) => {
+    const filter = req.query;
+    try {
+        const query = getFilterQuery(filter);
+        const result = await Temple.filterTemple(query)
+        if (result.length > 0)
+            res.status(200).json(result)
+        else
+            res.status(404).json({ message: "No temples found" })
+    } catch (err) {
+        res.status(500).json({message: "Error occurred while filtering temple" , error: err})
+    }
+}
+
+const getChardhamTemples = async (req, res) => {
+    try {
+        const temples = await Temple.GetChardham();
+        if (temples)
+            res.status(200).json(temples)
+        else
+            res.status(404).json({ message: "No temples found" })
+    } catch (err) {
+        res.status(500).json({ message: "Error occurred while retrieving Chardham temples", error: err });
+    }
+}
+
+module.exports = {getAllTemples, getTempleById, searchTemple, filterTemple, getChardhamTemples};
--- backend/src/query/templequeries.js
+++ backend/src/query/templequeries.js
@@ -4,9 +4,10 @@ const templeQueries = {
     AddTemple: `INSERT INTO public."temple" ("name", "description", "cover_image", "map_url", "reference_link", "god", "location", "importance", "images", "keywords") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;`,
     UpdateTemple: `UPDATE public."temple" SET "name" = $1, "description" = $2, "cover_image" = $3, "map_url" = $4, "reference_link" = $5, "god" = $6, "location " = $7, "importance" = $8, "images" = $9, "keywords" = $10 WHERE "_id" = $11 RETURNING *;`,
     DeleteTemple: `DELETE FROM public."temple" WHERE "_id" = $1 RETURNING *;`,
-    SearchTemple: `SELECT "name", "_id" FROM public."temple" WHERE "name" ILIKE "$1*";`,
+    SearchTemple: `SELECT "name", "_id" FROM public."temple" WHERE "name" ILIKE $1`,
     FilterTemple: `SELECT "_id", "cover_image", "name", "location" FROM public."temple" WHERE `,
-    GetChardham: `SELECT "_id", "cover_image", "name", "location" FROM public."temple" WHERE 'chardham' = ANY("keyword")`
+    FilterByKeyword: `SELECT "_id", "cover_image", "name", "location" FROM public."temple" WHERE "keyword" && $1;`,
+    GetChardham: `SELECT "_id", "cover_image", "name", "location" FROM public."temple" WHERE "keyword" && ARRAY['chardham'];`,
 }
 
 module.exports = {templeQueries}
