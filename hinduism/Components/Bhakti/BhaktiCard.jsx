87875fec4f8ad0d6767641524f9a662b97732f14
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2025-01-22 16:02:13 +0530

    `Added author parameter to BhaktiCard component and adjusted image width`

--- hinduism/Components/Bhakti/BhaktiCard.jsx
+++ hinduism/Components/Bhakti/BhaktiCard.jsx
@@ -1,6 +1,6 @@
 import { FaPlay, FaHeart, FaShare } from 'react-icons/fa'
 
-export default function BhaktiCard({ type, description, count, path, image }) {
+export default function BhaktiCard({ type, description, count, path, image, author }) {
 
   return (
     <div
@@ -8,10 +8,10 @@ export default function BhaktiCard({ type, description, count, path, image }) {
               className="flex items-center justify-between p-4 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow"
             >
               <div className="flex items-center gap-4">
-                <img src={image} className='w-[15%] md:w-[15%] lg:w-[10%]'></img>
+                <img src={image} className='w-[15%] md:w-[15%] lg:w-[20%]'></img>
                 <div>
                   <h3 className="font-semibold">{type}</h3>
-                  <p className='text-sm text-gray-600 mb-2'>Author name</p>
+                  <p className='text-sm text-gray-600 mb-2'>{author? author: "Unknown"}</p>
                   <p className="text-[12px] text-gray-500">{description.slice(0, 100) + "..."}</p>
                 </div>
               </div>
