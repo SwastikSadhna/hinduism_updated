764a8596aaf1d54d7bf4966dd6b8d0e38fd7caf1
Dhruv <sheredhruv@gmail.com>
2025-01-22 14:59:23 +0530

    `Refactor TempleFeatureImage component and BookDetailsPage component`

--- hinduism/Components/Temple/TempleFeatureImage.jsx
+++ hinduism/Components/Temple/TempleFeatureImage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const TempleImage = ({ src, alt }) => {
   return (
-    <div className="flex justify-center my-4">
-      <img src={src} alt={alt} className="rounded-lg shadow-lg w-full h-64 object-cover sm:w-1/2 md:w-1/3" />
+    <div className="flex justify-center my-4 w-full">
+      <img src={src} alt={alt} className="rounded-lg shadow-lg w-full object-cover sm:w-1/2 md:w-1/3" />
     </div>
   );
 };
--- hinduism/Pages/Book.jsx
+++ hinduism/Pages/Book.jsx
@@ -7,6 +7,18 @@ import axios from 'axios';
 import API_CONFIG from '../src/config/api';
 
 export default function BookDetailsPage() {
+
+  const [book, setBook] = new useState({})
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.get(API_CONFIG.baseUrl + `/book/id/${id}`).then((res) => {
+      setBook(res.data.data[0])
+    }).catch((err) => {
+      console.log(err);
+    })
+  }, [])
+  
   return (
     <div className="min-h-screen flex flex-col">
       <BookHeader
@@ -28,4 +40,3 @@ export default function BookDetailsPage() {
     </div>
   )
 }
-
