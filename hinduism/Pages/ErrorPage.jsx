dcee42519d872a1ec317c43f8d3cb009c4e83ec5
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2025-01-26 12:38:03 +0530

    `Added filter functionality to Books page`

--- hinduism/Pages/ErrorPage.jsx
+++ hinduism/Pages/ErrorPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { imageDetails } from "../resources";
 
 const ErrorPage = ({code, message}) => {
@@ -6,9 +7,11 @@ const ErrorPage = ({code, message}) => {
       <img src={imageDetails.error.src} width={200} className="ml-12"></img>
         <h1 className="text-8xl font-bold -mt-40 mb-20 ml-40" style={{color: "#6c63ff"}}>{ code }</h1>
         <h1 className="text-4xl font-bold mb-4" style={{ textDecoration:"underline"}}>{message}</h1>
-      <p className="text-lg mb-8">Oops! The page you're looking for doesn't exist or has been moved.</p>
+      <p className="text-lg mb-8">Oops! The page you're looking for doesn't exist or some internal error occured.</p>
       <div className="flex justify-center space-x-4">
-        
+        <Link to={"/"}>
+            <button className="bg-orange-400 p-3 rounded-md text-[1em] font-bold "> Back to Home</button>
+        </Link>
       </div>
     </div>
   </div>
