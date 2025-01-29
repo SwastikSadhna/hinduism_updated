9d7814a89940b5982e8d1735773b3eaed8ba3244
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-01-29 23:48:26 +0530

    `Added icons to resources and refactored dashboard to fetch stats asynchronously`

--- admin-panel/src/App.tsx
+++ admin-panel/src/App.tsx
@@ -2,6 +2,16 @@ import { Admin, ListGuesser, Resource, ShowGuesser } from "react-admin";
 import { Layout } from './Layout';
 import dataProvider from "./dataProvider";
 
+// Import Icons
+import BookIcon from "@mui/icons-material/Book";
+import TempleHinduIcon from "@mui/icons-material/TempleHindu";
+import PersonIcon from "@mui/icons-material/Person";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import CategoryIcon from "@mui/icons-material/Category";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
+import LibraryBooksIcon from "@mui/icons-material/List";
+
 import Dashboard from "./pages/dashboard";
 
 // show all
@@ -23,16 +33,16 @@ import bhaktiCreate from "./pages/bhakti/bhaktiCreate";
 
 const App = () => {
     return (
-        <Admin layout={Layout} dataProvider={dataProvider} dashboard={Dashboard}>
-            <Resource name="bhakti" list={bhaktiList} show={bhaktiShow} create={bhaktiCreate} />
-            <Resource name="categories" list={ListGuesser} show={ShowGuesser} />
-            <Resource name="book" list={bookList} show={BookShow} />
-            <Resource name="temple" list={templeList} show={TempleShow} />
-            <Resource name="avatar" list={avatarList} show={AvatarShow} />
-            <Resource name="god" list={godList} show={GodShow} />
-            <Resource name="sloka" list={ListGuesser} show={ShowGuesser} />
-            <Resource name="granth" list={ListGuesser} show={ShowGuesser} />
-            <Resource name="granthitem" list={ListGuesser} show={ShowGuesser} />
+        <Admin layout={Layout} dataProvider={dataProvider} dashboard={Dashboard} >
+            <Resource name="bhakti" list={bhaktiList} show={bhaktiShow} create={bhaktiCreate} icon={AutoStoriesIcon} />
+            <Resource name="categories" list={ListGuesser} show={ShowGuesser} icon={CategoryIcon} />
+            <Resource name="book" list={bookList} show={BookShow} icon={BookIcon} />
+            <Resource name="temple" list={templeList} show={TempleShow} icon={TempleHinduIcon} />
+            <Resource name="avatar" list={avatarList} show={AvatarShow} icon={PersonIcon} />
+            <Resource name="god" list={godList} show={GodShow} icon={CollectionsBookmarkIcon} />
+            <Resource name="sloka" list={ListGuesser} show={ShowGuesser} icon={MenuBookIcon} />
+            <Resource name="granth" list={ListGuesser} show={ShowGuesser} icon={LibraryBooksIcon} />
+            <Resource name="granthitem" list={ListGuesser} show={ShowGuesser} icon={LibraryBooksIcon} />
         </Admin>
     );
 };
--- admin-panel/src/pages/dashboard.tsx
+++ admin-panel/src/pages/dashboard.tsx
@@ -1,20 +1,39 @@
 import { Card, CardContent, Typography, Box } from "@mui/material";
 import dataProvider from "../dataProvider";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
 
-    const bhakti = dataProvider.getList("bhakti", 0);
-    console.log(bhakti)
+    const [stats, setStats] = useState([
+        { title: "Bhakti", value: 0 },
+        { title: "Book", value: 0 },
+        { title: "Temple", value: 0 },
+        { title: "Avatar", value: 0 },
+        { title: "God", value: 0 },
+        { title: "Sloka", value: 0 },
+        { title: "Granth", value: 0 },
+    ]);
 
-  const stats = [
-    { title: "Bhakti", value: "10"},
-    { title: "Book", value: "24"},
-    { title: "Temple", value: "27"},
-    { title: "Avatar", value: "54"},
-    { title: "God", value: "44"},
-    { title: "Sloka", value: "20"},
-    { title: "Granth", value: "5"},
-  ];
+    useEffect(() => {
+        const fetchStats = async () => {
+            try {
+                const updatedStats = await Promise.all(stats.map(async (stat) => {
+                    const result = await dataProvider.getList(stat.title, { 
+                        pagination: { page: 1, perPage: 10 }, 
+                        sort: { field: "id", order: "ASC" } 
+                    });
+                    return { ...stat, value: result.total };
+                }));
+
+                setStats(updatedStats);
+            } catch (error) {
+                console.error("Error fetching stats:", error);
+            }
+        };
+
+        fetchStats();
+    }, []);
+
 
   return (
     <Box sx={{ p: 1, display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))", gap: 1 }}>
