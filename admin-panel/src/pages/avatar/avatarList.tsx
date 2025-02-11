69409d2178ac0cef0f0f40fe579443425dbb59a2
Vinayrk2 <v277772@gmail.com>
2025-02-10 21:35:37 -0800

    `Removed 8 fields from AvatarList Datagrid`

--- admin-panel/src/pages/avatar/avatarList.tsx
+++ admin-panel/src/pages/avatar/avatarList.tsx
@@ -7,14 +7,8 @@ const AvatarList = () => (
             <TextField source="name" />
             <TextField source="image" />
             <TextField source="yuga" />
-            <TextField source="books" />
-            <TextField source="description" />
-            <TextField source="festival" />
             <TextField source="tamples" />
             <NumberField source="god_ref" />
-            <TextField source="sloka" />
-            <TextField source="festivals" />
-            <TextField source="reference_links" />
         </Datagrid>
     </List>
 );
